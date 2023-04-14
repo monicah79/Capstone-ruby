8813b9da881d0fa1986d92e0f73e925d43e8b3d4
Cabdifataax Yaasiin <41564330+AbdifatahYasin1@users.noreply.github.com>
2023-04-14 12:29:44 +0300

    Update game_spec.rb

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,41 +1,31 @@
-require_relative '../genre'
-require_relative '../music'
+require_relative '../game'
 
-RSpec.describe Genre do
-  before :each do
-    @genre = Genre.new('Rock')
-    @music_album = MusicAlbum.new('Hotel California', '1976-12-08', on_spotify: true)
-  end
+RSpec.describe Game do
+    describe '#initialize' do
+        let(:game) { Game.new('Super Mario', true, '2020/10/10', '1990/10/10') }
 
-  it 'Check if it is an instance of the class' do
-    expect(@genre).to be_instance_of Genre
-  end
+        it 'should set an id' do
+            expect(game.id).to_not be_nil
+        end
 
-  it 'Check if it has a name attribute' do
-    expect(@genre.name).to eq 'Rock'
-  end
+        it 'should set the title' do
+            expect(game.title).to eq('Super Mario')
+        end
 
-  it 'Check if it has an items attribute' do
-    expect(@genre.items).to eq []
-  end
+        it 'should set the multiplayer' do
+            expect(game.multiplayer).to eq(true)
+        end
 
-  it 'Check if it can add an item to its collection' do
-    @genre.add_item(@music_album)
-    expect(@genre.items).to include @music_album
-  end
+        it 'should set the last played at' do
+            expect(game.last_played_at).to eq('2020/10/10')
+        end
 
-  it 'Check if it adds itself as the item\'s genre' do
-    @genre.add_item(@music_album)
-    expect(@music_album.genre).to eq @genre
-  end
+        it 'should set the publish date' do
+            expect(game.publish_date).to eq('1990/10/10')
+        end
 
-  it 'Check if it adds the item to its items collection' do
-    @genre.add_item(@music_album)
-    expect(@genre.items).to include @music_album
-  end
-
-  it 'Check if it adds itself to the item\'s genres collection' do
-    @genre.add_item(@music_album)
-    expect(@music_album.genres).to include @genre
-  end
-end
+        it 'should set an empty array for authors' do
+            expect(game.authors).to be_empty
+        end
+    end
+  
